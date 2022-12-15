40edc5c12f2abe055e8a02a990a6b6aa60874dde
Mariangel86 <Mariangel_antequera@hotmail.com>
2022-12-14 22:22:49 -0500

    changes of wednesday

--- src/componentes/ItemDetalleContenedor.js
+++ src/componentes/ItemDetalleContenedor.js
@@ -1,13 +1,10 @@
 import React, {useState,useEffect} from "react";
 import ItemDetalle from "./ItemDetalle";
 import { useParams } from "react-router-dom";
+import Locura from '../assets/imagenlocura.png';
 
-const ejemplos2= [
-  {id:1, title: "Taza Mug", image:"https://grafico.online/wp-content/uploads/2021/05/Mug-1.png", category:'Mugs'},
-    {id:2, title:"fantasy", image:"https://images.emojiterra.com/twitter/v13.1/512px/1f92a.png", category:'Fantasy'},
-    {id:3, title:"anime", image:"https://revistadiners.com.co/wp-content/uploads/2019/09/portada_anime_1200x800.jpg", category:'Anime'}
-
-  ];
+const ejemplos2= 
+    {id:4, title: "locura", image:<Locura/>, category: 'Mugs' };
 
     
 const ItemDetalleContenedor=()=>{
