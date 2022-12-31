77af6617190c65c07f21a39be801c25508ab46c4
Mariangel86 <Mariangel_antequera@hotmail.com>
2022-12-30 23:06:14 -0500

    changes of friday

--- src/componentes/ItemDetalleContenedor.js
+++ src/componentes/ItemDetalleContenedor.js
@@ -1,13 +1,15 @@
 import React, {useState,useEffect} from "react";
 import ItemDetalle from "./ItemDetalle";
 import { useParams } from "react-router-dom";
-import Locura from '../assets/imagenlocura.png';
-import Carrito from '../assets/carrito.png';
+//import Locura from '../assets/imagenlocura.png';
+//import Carrito from '../assets/carrito.png';
+import { doc, getDoc} from 'firebase/firestore';
+import {db}from './../firebase/firebaseConfig';
 
-const ejemplos2= [
-  {id:1, price: 50, title: "locura", image: (Locura), category: 'Mugs' },
-  {id:2, price: 100, title: "carrito", image: (Carrito), category: 'Fantasy' }
-]
+//const ejemplos2= [
+  //{id:1, price: 50, title: "locura", image: (Locura), category: 'Mugs' },
+ // {id:2, price: 100, title: "carrito", image: (Carrito), category: 'Fantasy' }
+//]
 
     
 const ItemDetalleContenedor=()=>{
@@ -15,13 +17,19 @@ const ItemDetalleContenedor=()=>{
     const {detalleId}= useParams();
 
     useEffect(() => {
+      const queryDoc = doc(db, 'productos', '0DwN5cnlsVF59eUv4Lub');
+     getDoc(queryDoc)
+          .then (resl=> CambiarProductos({id: resl.id, ...resl.productos()}))
+  },[detalleId])
+
+   (/* useEffect(() => {
         const establecerProductos = new Promise(resolve => {
           setTimeout(() => {
             resolve(ejemplos2);
            },3000);
           });
             establecerProductos.then (res=> CambiarProductos(res.find(ejemplo2 => ejemplo2.id === parseInt(detalleId))));
-    },[detalleId])
+    },[detalleId])*/)
 
     return(
     <ItemDetalle productos={productos}/>
--- src/componentes/ItemListContainer.js
+++ src/componentes/ItemListContainer.js
@@ -1,40 +1,59 @@
 import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
-import Locura from '../assets/imagenlocura.png';
-import Items from './Items';
-import Carrito from '../assets/carrito.png';
+//import Locura from '../assets/imagenlocura.png';
+//import Items from './Items';
+//import Carrito from '../assets/carrito.png';
+import { collection, getDocs, query, where } from "firebase/firestore";
+import {db} from './../firebase/firebaseConfig';
 
-    const ejemplos2=[
-          {id:1, price: 50, title: "locura", image: (Locura), category: 'Mugs' },
-          {id:2, price:100, title: "carrito", image: (Carrito), category: 'Fantasy' }
-        ]
+  //  const ejemplos2=[
+  //        {id:1, price: 50, title: "locura", image: (Locura), category: 'Mugs' },
+  //        {id:2, price:100, title: "carrito", image: (Carrito), category: 'Fantasy' }
+  //      ]
     
           
       const ItemLista=()=>{
           const [productos, CambiarProductos ] = useState ([]);
+
           const {categoriaId}= useParams();
-      
+
           useEffect(() => {
-              const establecerProductos = new Promise(resolve => {
-                setTimeout(() => {
-                  resolve(ejemplos2);
-                 },3000);
-                });
-                if (categoriaId){
-                  establecerProductos.then(res=> CambiarProductos(res.filter(ejemplo2=> ejemplo2.category === categoriaId)));
-            
-                }else{
-                  
-                  establecerProductos.then(res => CambiarProductos(res));
-                }
-                
-            },[categoriaId])
-      
-          return(
-          <Items productos={productos}/>
-          );
-              
+            const queryCollection = collection(db, 'productos');
+            if (categoriaId) {
+            const queryFilter= query(queryCollection,
+              where('category', '==', categoriaId))
+
+           getDocs(queryFilter)
+                .then (resl=> CambiarProductos(resl.docs.map(producto=>({id: producto.id, ...producto.productos() }))))
+          }else{
+            getDocs(queryCollection)
+                .then (resl=> CambiarProductos(resl.docs.map(producto=>({id: producto.id, ...producto.productos() }))))
+          }
+
+        },[categoriaId])
       }
+      
+        //  useEffect(() => {
+        //      const establecerProductos = new Promise(resolve => {
+        //        setTimeout(() => {
+        //          resolve(ejemplos2);
+        //         },3000);
+        //        });
+        //         if (categoriaId){
+        //          establecerProductos.then(res=> CambiarProductos(res.filter(ejemplo2=> ejemplo2.category === categoriaId)));
+        //    
+        //        }else{
+        //          
+        //          establecerProductos.then(res => CambiarProductos(res));
+        //        }
+        //        
+        //    },[categoriaId])
+        //
+        //  return(
+        //  <Items productos={productos}/>
+        //  );
+        //      
+        //}
       
 
 
