745d345fbc1a95d2dcbcebb1c8a5f0b323fc76da
Mariangel86 <Mariangel_antequera@hotmail.com>
2023-01-27 22:22:40 -0500

    changes of friday

--- src/componentes/InicioSesion.js
+++ src/componentes/InicioSesion.js
@@ -43,7 +43,7 @@ const InicioSesion=()=> {
   
     try {
       await signInWithEmailAndPassword (auth,correo,password);
-      navigate('/Iniciarsesion');
+      navigate('/');
     }  catch (error){
       cambiarEstadoAlerta(true);
 
